bcb075255b9b01ecc5f6a74438d435c661d84ff5
Ava <transorsmth@gmail.com>
2025-02-05 15:30:17 -0500

    Checking if prefetch_related helps speed

--- api/face_classify.py
+++ api/face_classify.py
@@ -42,7 +42,7 @@ def cluster_faces(user, inferred=True):
     paginator = Paginator(faces, 5000)
 
     for page in range(1, paginator.num_pages + 1):
-        for face in paginator.page(page).object_list:
+        for face in paginator.page(page).object_list.prefetch_related():
             if ((not face.person) or inferred) and face.encoding:
                 face_encoding.append(face.get_encoding_array())
 
